eeb885f38b33d12d8fc462d2cdf74db37b981f2b
4M01 <2amolchavan@gmail.com>
2024-05-21 17:29:57 +0530

    chore: added few more asssertions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,5 +6,16 @@ test("GET booking summary", async ({ request }) => {
 
   expect(response.status()).toBe(200);
   const body = await response.json();
-  console.log(JSON.stringify(body));
+  expect(body.bookings.length).toBeGreaterThanOrEqual(1);
+  expect(isValidDate(body.bookings[0].bookingDates.checkin)).toBe(true);
+  expect(isValidDate(body.bookings[0].bookingDates.checkout)).toBe(true);
+
 });
+function isValidDate(date: string): any {
+  if (Date.parse(date)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
